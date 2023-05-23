e58c4998688186acd9ae97af59423eb55366ad98
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-05-23 08:00:33 +0200

    skills styling

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,26 +3,46 @@ import './css/skillscss.css'
 
 export const Skills = () => {
   return (
+    <section className="skills">
 
-    <div className="techskills">
+      <h3>
+    Skills
+      </h3>
+      <dl className="code">
+        <dt>Code</dt>
+        <dd>HTML5</dd>
+        <dd>CSS3</dd>
+        <dd> <span className="tech js">JavaScript ES6</span></dd>
+        <dd>React.js</dd>
+        <dd>Redux</dd>
+        <dd>MongoDB</dd>
+        <dd>Node.js</dd>
+        <dd>REST API</dd>
+      </dl>
 
-      <div className=" techtitle">
-        <h2>Skills</h2>
-      </div>
+      <dl className="toolbox">
+        <dt>Toolbox</dt>
+        <dd>VSCode</dd>
+        <dd>Github</dd>
+        <dd><span className="tech Cloud">Google Cloud</span></dd>
+        <dd>Postman</dd>
+        <dd>Figma</dd>
+        <dd>Canva</dd>
+        <dd>Slack</dd>
 
-      <div className="techchild">
+      </dl>
 
-        <p>HTML, CSS, Flexbox,
-          <span className="tech js">JavaScript</span>,
-         ES6, JSX,<span className="tech react">React</span>
-         Hooks, Redux, Node.js,
-         Mongo DB, Web Accessibly,
-          API:s, <span className="tech mob">mob-programming</span>,
-          pair-programming, Github.
-        </p>
+      <dl className="morelist">
+        <dt className="more">More</dt>
+        <dd>Project management</dd>
+        <dd>Agile Methodology</dd>
+        <dd> <span className="tech acces">Web Accessibly</span> </dd>
+        <dd>mob-programming</dd>
+        <dd>pair-programming</dd>
 
-      </div>
+      </dl>
 
-    </div>
+    </section>
+
   )
 }
