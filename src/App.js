083721b4bc2a6fa75b5f69479a6464603e874a1f
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-05-23 07:36:10 +0200

    skills

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Main } from './components/main'
+import { Skills } from './components/skills'
 
 export const App = () => {
   return (
     <div>
       <Main />
+      <Skills />
     </div>
   )
 }
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './css/skillscss.css'
 
-export const App = () => {
+export const Skills = () => {
   return (
-    <div>
-      Movimiento
+
+    <div className="techskills">
+
+      <div className=" techtitle">
+        <h2>Skills</h2>
+      </div>
+
+      <div className="techchild">
+
+        <p>HTML, CSS, Flexbox,
+          <span className="tech js">JavaScript</span>,
+         ES6, JSX,<span className="tech react">React</span>
+         Hooks, Redux, Node.js,
+         Mongo DB, Web Accessibly,
+          API:s, <span className="tech mob">mob-programming</span>,
+          pair-programming, Github.
+        </p>
+
+      </div>
+
     </div>
   )
 }
