2856b33520d365aff24a344856b10484244ae211
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-05-26 11:47:32 +0200

    overrule formspree

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,6 +7,7 @@ import blackLinkedin from '../images/blacklinkedinicon.svg';
 export const Contact = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -14,6 +15,26 @@ export const Contact = () => {
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        setIsSubmitted(true);
+      } else {
+        console.error('Form submission failed');
+      }
+    };
+    xhr.send(data);
   };
 
   return (
@@ -28,26 +49,33 @@ export const Contact = () => {
           </div>
         </div>
         <div className="info-container">
-          <form action="https://formspree.io/f/mrgvvawb" method="POST">
-            <label htmlFor="email">Your email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              required />
+          {!isSubmitted ? (
+            <form
+              action="https://formspree.io/f/mrgvvawb"
+              method="POST"
+              onSubmit={handleSubmit}>
+              <label htmlFor="email">Your email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                required />
 
-            <label htmlFor="message">Message:</label>
-            <textarea
-              name="message"
-              id="message"
-              value={message}
-              onChange={handleMessageChange}
-              placeholder="Write your message here"
-              required />
-            <button type="submit">Send</button>
-          </form>
+              <label htmlFor="message">Message:</label>
+              <textarea
+                name="message"
+                id="message"
+                value={message}
+                onChange={handleMessageChange}
+                placeholder="Write your message here"
+                required />
+              <button type="submit">Send</button>
+            </form>
+          ) : (
+            <p>Message sent successfully!</p>
+          )}
         </div>
         <div className="icons">
           <div className="iconsgithub">
